ac16492cd282037a8e3b9f7dac1f8b5595c6e870
sikarwar2010 <sikarwar2010@gmail.com>
2025-09-26 00:38:19 +0530

    feat:clerk auth & convex integration

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+
+export default function AuthLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className='flex flex-col justify-center items-center h-screen'>
+            {children}
+        </div>
+    );
+}
