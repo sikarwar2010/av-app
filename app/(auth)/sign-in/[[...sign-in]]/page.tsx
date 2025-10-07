4be830893552c871aee2c58c11a4afbd6688d211
sikarwar2010 <sikarwar2010@gmail.com>
2025-10-07 08:57:42 +0530

    feat:some change in sign & signup

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,17 +2,6 @@ import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
     return (
-        <SignIn
-            afterSignInUrl="/dashboard"
-            afterSignUpUrl="/dashboard"
-            signUpUrl="/sign-up"
-            redirectUrl="/dashboard"
-            appearance={{
-                elements: {
-                    formButtonPrimary: "bg-primary hover:bg-primary/90",
-                    card: "shadow-lg",
-                }
-            }}
-        />
+        <SignIn />
     )
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,17 +2,6 @@ import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
     return (
-        <SignUp
-            afterSignInUrl="/dashboard"
-            afterSignUpUrl="/dashboard"
-            signInUrl="/sign-in"
-            redirectUrl="/dashboard"
-            appearance={{
-                elements: {
-                    formButtonPrimary: "bg-primary hover:bg-primary/90",
-                    card: "shadow-lg",
-                }
-            }}
-        />
+        <SignUp />
     )
 }
